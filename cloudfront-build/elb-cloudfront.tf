ad6228adc10af42da75c872ae24734e879773550
DohaHQ <108454921+DohaHQ@users.noreply.github.com>
2022-09-18 19:29:08 +0300

    Update elb-cloudfront.tf

--- cloudfront-build/elb-cloudfront.tf
+++ cloudfront-build/elb-cloudfront.tf
@@ -1,12 +1,14 @@
-# https://registry.terraform.io/providers/hashicorp/aws/3.74.2
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "= 3.74.2"
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
+
 provider "aws" {
   region = "us-east-1"
 }
