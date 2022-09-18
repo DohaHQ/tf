8c4bdf101c5823d8488546e3420877f981a8a20d
DohaHQ <108454921+DohaHQ@users.noreply.github.com>
2022-09-18 20:31:11 +0300

    Update elb-cloudfront.tf

--- cloudfront-build/elb-cloudfront.tf
+++ cloudfront-build/elb-cloudfront.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  shared_config_files=["/home/ubuntu/.aws/config"]
+  shared_config_files = ["/home/ubuntu/.aws/config"]
   shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
   profile = "default"
 }
